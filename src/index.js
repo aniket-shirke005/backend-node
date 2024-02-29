051bff833188a266abefcb404436e7655872263d
fdev3 <aniketbshirke07@gmail.com>
2024-02-29 10:39:57 +0530

    basic express app setup with mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({path:'./env'})
 
-connectDB()
+const port = process.env.PORT || 8000
+connectDB().then(()=>{
+    app.on('error',(error)=>{
+        console.log(`ERR : ${error}`)
+    })
+    app.listen(port,()=>{
+        console.log(`App started on port: ${port}`)
+    })
+
+}).catch((err)=> console.error(`Mongodb connection error ${err}`))
